e660c6375b5d6ea6f0b6e654760a316fb0656786
MK-v5 <mksomna@st.noorderpoort.nl>
2024-06-07 09:30:08 +0200

    Inital commit

--- Program.cs
+++ Program.cs
@@ -6,15 +6,21 @@
         * Maak een kaartspel template met een class of struct 
         * voor een kaart en een class voor een deck
         * (een set van een x-aantal kaarten).
-
+        * 
+        *-=================================================================-
+        *
         * Zorg voor logische invullingen van beide classes en/of structs.
         * Wanneer een deck geinstantieerd wordt
         * moet het gevuld worden met de juiste 52 kaarten.
-
+        * 
+        *-=================================================================-
+        *
         * Zorg voor duidelijk commentaar op de juiste plekken.
         * Vraag om de speler naam en gebruik
         * deze in je communicatie met de speler.
-
+        * 
+        *-=================================================================-
+        *
         * Zorg voor een game loop die pas ophoudt als de speler de
         * >>esc-toets<< indrukt,
         * dit is de enige mogelijkheid om de applicatie te laten eindigen. 
